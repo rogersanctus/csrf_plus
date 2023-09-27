cc9f853c29795109d0e493d11b9d955020f37211
rogersanctus <rogersanctus@gmail.com>
2023-09-27 01:39:45 -0300

    Makes the token_config_fixture accept other options to merge with default options

--- test/support/fixtures.ex
+++ test/support/fixtures.ex
@@ -1,8 +1,10 @@
 defmodule CsrfPlus.Fixtures do
-  def token_config_fixture() do
-    Application.put_env(:csrf_plus, CsrfPlus.Token,
+  def token_config_fixture(other_options \\ []) do
+    options = [
       secret_key:
         "XeMI8JB7othiZ3YOD4aCfSs_L4LZ1FmOaFtE3lFgjQN9cNxWBMsdJcxzp83TIuMcXbKAh1h0jb5zlybAPheTlA=="
-    )
+    ]
+
+    Application.put_env(:csrf_plus, CsrfPlus.Token, Keyword.merge(options, other_options))
   end
 end
