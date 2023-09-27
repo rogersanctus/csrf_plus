1b707cf3d2bcfa7d9f090c8c72200e331f92aefc
rogersanctus <rogersanctus@gmail.com>
2023-09-27 00:35:32 -0300

    Removes token related test logic from the CsrfPlus tests

--- test/csrf_plus/csrf_plus_test.exs
+++ test/csrf_plus/csrf_plus_test.exs
@@ -106,47 +106,6 @@ defmodule CsrfPlus.CsrfPlusTest do
     end
   end
 
-  describe "CSRF tokens generation" do
-    test "if it can generate a token" do
-      conn = build_conn(:get)
-
-      {token, signed} = CsrfPlus.generate_token(conn)
-
-      refute is_nil(token)
-      refute is_nil(signed)
-    end
-
-    test "if it fails to generate a token when the conn has no secret key" do
-      conn = conn(:get, "/")
-
-      assert_raise RuntimeError, fn -> CsrfPlus.generate_token(conn) end
-    end
-
-    test "if a generated token is signed and verifyable" do
-      conn = build_conn(:get)
-      {token, signed} = CsrfPlus.generate_token(conn)
-      result = CsrfPlus.verify_token(conn, signed)
-
-      assert match?({:ok, ^token}, result)
-    end
-
-    test "if verifying a token fails with invalid token when token is invalid" do
-      conn = build_conn(:get)
-
-      wrong_token = "wrong token"
-      result = CsrfPlus.verify_token(conn, wrong_token)
-      assert match?({:error, "invalid token"}, result)
-    end
-
-    test "if verifying a token fails when the conn has no secret key" do
-      conn = conn(:get, "/")
-
-      any_token = "any token"
-      result = CsrfPlus.verify_token(conn, any_token)
-      assert match?({:error, "no secret key provided in the Plug conn"}, result)
-    end
-  end
-
   describe "CsrfPlus validations" do
     test "if it can put a token into the session" do
       Mox.stub_with(CsrfPlus.StoreMock, CsrfPlus.OkStoreMock)
