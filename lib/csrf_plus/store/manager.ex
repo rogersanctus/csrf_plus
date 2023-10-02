4b9e6eb5e79997481856878bdfc317f13299c689
rogersanctus <rogersanctus@gmail.com>
2023-10-02 18:33:42 -0300

    Created the test to check if the store is called properly after the checking time and makes the needed fixes in the Manager module

--- lib/csrf_plus/store/manager.ex
+++ lib/csrf_plus/store/manager.ex
@@ -1,14 +1,18 @@
 defmodule CsrfPlus.Store.Manager do
+  require Logger
   use GenServer
 
   # Check age from hour to hour
   @check_age_time 60 * 60 * 1000
 
-  def start_link(
-        [token_max_age: token_max_age] =
-          init_arg
-      )
-      when is_integer(token_max_age) do
+  def start_link(init_arg) when is_list(init_arg) do
+    if Keyword.get(init_arg, :token_max_age, nil) == nil do
+      raise "CsrfPlus.Store.Manager requires token_max_age to be set"
+    end
+
+    check_time = Keyword.get(init_arg, :check_age_time, @check_age_time)
+
+    init_arg = Keyword.put(init_arg, :check_age_time, check_time)
     GenServer.start_link(__MODULE__, init_arg, name: __MODULE__)
   end
 
@@ -17,8 +21,8 @@ defmodule CsrfPlus.Store.Manager do
     {:ok, state, {:continue, :check_age}}
   end
 
-  def handle_continue(:check_age, state) do
-    Process.send_after(self(), :check_age, @check_age_time)
+  def handle_continue(:check_age, %{check_age_time: check_time} = state) do
+    Process.send_after(self(), :check_age, check_time)
 
     {:noreply, state}
   end
@@ -26,7 +30,8 @@ defmodule CsrfPlus.Store.Manager do
   def handle_info(
         :check_age,
         %{
-          token_max_age: token_max_age
+          token_max_age: token_max_age,
+          check_age_time: check_time
         } = state
       ) do
     config = Application.get_env(:csrf_plus, CsrfPlus, [])
@@ -34,7 +39,7 @@ defmodule CsrfPlus.Store.Manager do
 
     if !is_nil(store) do
       check_age(store, token_max_age)
-      Process.send_after(self(), :check_age, @check_age_time)
+      Process.send_after(self(), :check_age, check_time)
     end
 
     {:noreply, state}
@@ -45,6 +50,10 @@ defmodule CsrfPlus.Store.Manager do
   end
 
   defp check_age(store, token_max_age) do
-    store.delete_dead_accesses(token_max_age)
+    if Kernel.function_exported?(store, :delete_dead_accesses, 1) do
+      store.delete_dead_accesses(token_max_age)
+    else
+      Logger.warning("CsrfPlus.Store.Manager :delete_dead_accesses not implemented")
+    end
   end
 end
--- test/csrf_plus/store/manager_test.exs
+++ test/csrf_plus/store/manager_test.exs
@@ -2,6 +2,12 @@ defmodule CsrfPlus.Store.ManagerTest do
   alias CsrfPlus.Store.Manager
   use ExUnit.Case
 
+  setup _ do
+    on_exit(fn ->
+      Application.delete_env(:csrf_plus, CsrfPlus)
+    end)
+  end
+
   test "if the Store Manager is started" do
     Manager.start_link(token_max_age: 1000)
 
@@ -12,8 +18,22 @@ defmodule CsrfPlus.Store.ManagerTest do
   end
 
   test "if the store is not started when missing params" do
-    assert_raise FunctionClauseError, fn ->
+    assert_raise RuntimeError, fn ->
       Manager.start_link([])
     end
+  end
+
+  test "if the Store is called after the Manager checking time" do
+    Application.put_env(:csrf_plus, CsrfPlus, store: CsrfPlus.StoreMock)
+
+    Manager.start_link(token_max_age: 1000, check_age_time: 20)
+
+    manager_pid = Process.whereis(Manager)
+    test_pid = self()
+
+    Mox.stub(CsrfPlus.StoreMock, :delete_dead_accesses, fn _ -> send(test_pid, :store_called) end)
+    Mox.allow(CsrfPlus.StoreMock, test_pid, manager_pid)
+
+    assert_receive :store_called
   end
 end
