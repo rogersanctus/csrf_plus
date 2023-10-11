6a30e7b1f0bece184e108dadc77b6c293d1d1c88
rogersanctus <rogersanctus@gmail.com>
2023-10-11 02:06:07 -0300

    Adds another defmock to Store. This one has the optional callback skipped

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,8 @@
 ExUnit.start()
 
 Mox.defmock(CsrfPlus.StoreMock, for: CsrfPlus.Store.Behaviour)
+
+Mox.defmock(CsrfPlus.OptionalStoreMock,
+  for: CsrfPlus.Store.Behaviour,
+  skip_optional_callbacks: true
+)
